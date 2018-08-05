f25ddad1facfa46bcc9048bc614a1a3a5945bb0b
Mohammed Lazhari <lazhari.mohammed@outlook.com>
2018-08-05 11:03:12 +0100

    Testing the event handler on server side rendering

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Home = () => {
-    return <h1>I'm the Best component</h1>;
+    return (
+        <div>
+            <div>
+                <h1>I'm the Best component</h1>
+            </div>
+            <button onClick={() => console.log('Hi there!')}>Press me!</button>
+        </div>
+    );
 };
 
 export default Home;
